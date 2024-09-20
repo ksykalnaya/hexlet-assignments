1a9297f2c12eb327c27c81e678107c8d8ed152d7
ruaklkg <ksy-kalnaya@yandex.ru>
2024-09-20 12:20:40 +0300

    homework "dependency injection"

--- java-spring-ru/dependency-injection/src/main/java/exercise/controller/CommentsController.java
+++ java-spring-ru/dependency-injection/src/main/java/exercise/controller/CommentsController.java
@@ -1,5 +1,7 @@
 package exercise.controller;
 
+import exercise.model.Post;
+import exercise.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,5 +21,45 @@ import exercise.repository.CommentRepository;
 import exercise.exception.ResourceNotFoundException;
 
 // BEGIN
+@RestController
+@RequestMapping("/comments")
+public class CommentsController {
+    @Autowired
+    private CommentRepository commentRepository;
 
+    @GetMapping
+    public List<Comment> getComments(){
+        return commentRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Comment getComment(@PathVariable long id){
+        return commentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Comment " + id + " not found"));
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Comment createComment(@RequestBody Comment data){
+        return commentRepository.save(data);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Comment updateComment(@PathVariable long id, @RequestBody Comment data){
+        Comment comment = commentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Comment " + id + " not found"));
+        comment.setBody(data.getBody());
+        commentRepository.save(comment);
+        return comment;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteComment(@PathVariable long id){
+        commentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Post " + id + " not found"));
+        commentRepository.deleteById(id);
+    }
+}
 // END
--- java-spring-ru/dependency-injection/src/main/java/exercise/controller/PostsController.java
+++ java-spring-ru/dependency-injection/src/main/java/exercise/controller/PostsController.java
@@ -13,11 +13,56 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.http.HttpStatus;
 import java.util.List;
+import java.util.Optional;
 
 import exercise.model.Post;
 import exercise.repository.PostRepository;
 import exercise.exception.ResourceNotFoundException;
 
 // BEGIN
+@RestController
+@RequestMapping("/posts")
+public class PostsController {
+    @Autowired
+    private PostRepository postRepository;
+    @Autowired
+    private CommentRepository commentRepository;
 
+    @GetMapping
+    public List<Post> getPosts(){
+        return postRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Post getPost(@PathVariable long id){
+        return postRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Post with id " + id + " not found"));
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Post createPost(@RequestBody Post data){
+         return postRepository.save(data);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Post updatePost(@PathVariable long id, @RequestBody Post data){
+        Post post = postRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Post with id " + id + " not found"));
+        post.setTitle(data.getTitle());
+        post.setBody(data.getBody());
+        postRepository.save(post);
+        return post;
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePost(@PathVariable long id){
+        postRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Post " + id + " not found"));
+        postRepository.deleteById(id);
+        commentRepository.deleteByPostId(id);
+    }
+}
 // END
