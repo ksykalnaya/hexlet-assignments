494766e8c620b576b1ea094477936def7b3905ac
@hexlet/cli <support@hexlet.io>
2024-09-24 15:38:00 +0300

    submit java-spring-ru/dto

--- java-spring-ru/dto/src/main/java/exercise/controller/PostsController.java
+++ java-spring-ru/dto/src/main/java/exercise/controller/PostsController.java
@@ -1,9 +1,12 @@
 package exercise.controller;
 
+import exercise.model.Comment;
 import exercise.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
@@ -15,5 +18,46 @@ import exercise.dto.PostDTO;
 import exercise.dto.CommentDTO;
 
 // BEGIN
+@RestController
+@RequestMapping("/posts")
+public class PostsController {
 
+    @Autowired
+    PostRepository postRepository;
+    @Autowired
+    CommentRepository commentRepository;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<PostDTO> getPosts(){
+        var posts = postRepository.findAll().stream().map(this::setDTO).toList();
+        return posts;
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public PostDTO getPost(@PathVariable long id){
+        var post = postRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Post with id "  + id + " not found"));
+        return setDTO(post);
+    }
+
+    private PostDTO setDTO(Post post){
+        var comments = commentRepository.findByPostId(post.getId()).stream()
+                .map(this::setCommentDTO).toList();
+        var pDTO = new PostDTO();
+        pDTO.setId(post.getId());
+        pDTO.setBody(post.getBody());
+        pDTO.setTitle(post.getTitle());
+        pDTO.setComments(comments);
+        return pDTO;
+    }
+
+    private CommentDTO setCommentDTO(Comment comment){
+        var cDTO = new CommentDTO();
+        cDTO.setId(comment.getId());
+        cDTO.setBody(comment.getBody());
+        return cDTO;
+    }
+}
 // END
--- java-spring-ru/dto/src/main/java/exercise/dto/PostDTO.java
+++ java-spring-ru/dto/src/main/java/exercise/dto/PostDTO.java
@@ -5,5 +5,16 @@ import lombok.Getter;
 import lombok.Setter;
 
 // BEGIN
+@Getter
+@Setter
+public class PostDTO{
+    private long id;
+
+    private String title;
+
+    private String body;
+
+    private List<CommentDTO> comments;
+}
 
 // END
